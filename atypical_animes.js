d013d726ba2ce1105c35adfc70092368e369e5a2
emiliax16 <efpg1608@uc.cl>
2023-11-25 21:55:37 -0300

    un avance gente

---
+++ atypical_animes.js
@@ -0,0 +1,72 @@
+const marginVis3 = {top: 10, right: 60, bottom: 30, left: 60},
+    widthVis3 = 800 - marginVis3.left - marginVis3.right,
+    heightVis3 = 800 - marginVis3.top - marginVis3.bottom;
+
+const svgVis3 = d3.select("#vis-3")
+    .append("svg")
+    .attr("width", widthVis3)
+    .attr("height", heightVis3 + marginVis3.top + marginVis3.bottom)
+    .append("g")
+    .attr("transform", "translate(" + marginVis3.left + "," + marginVis3.top + ")");
+
+const tooltipVis3 = d3.select("#vis-3")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+    .style("position", "absolute");
+
+function updateVisualization(voteThreshold, ratingThreshold) {
+    d3.csv("anime.csv").then(function(data) {
+        let filteredData = data.filter(d => {
+            return +d["Number Votes"] >= voteThreshold && +d["Rating Score"] <= ratingThreshold;
+        });
+
+        svgVis3.selectAll("*").remove();
+
+        let root = d3.hierarchy({children: filteredData}).sum(d => +d["Number Votes"]);
+        d3.treemap().size([widthVis3, heightVis3]).padding(2)(root);
+
+        const colorScale = d3.scaleSequential(d3.interpolateBlues).domain([0, ratingThreshold]);
+        const blocks = svgVis3.selectAll("rect")
+            .data(root.leaves())
+            .enter().append("rect")
+            .attr('x', d => d.x0)
+            .attr('y', d => d.y0)
+            .attr('width', d => d.x1 - d.x0)
+            .attr('height', d => d.y1 - d.y0)
+            .style("stroke", "black")
+            .style("fill", d => colorScale(d.data["Rating Score"]))
+            .on("mouseover", function(event, d) {
+                tooltipVis3.transition().duration(200).style("opacity", 0.9);
+                tooltipVis3.html("Anime: " + d.data["Name"] + "<br/>" + "Votes: " + d.data["Number Votes"] + "<br/>" + "Rating: " + d.data["Rating Score"])
+                    .style("left", (event.pageX) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltipVis3.transition().duration(200).style("opacity", 0);
+            });
+
+        blocks.exit().remove();
+
+    }).catch(function(error){
+        console.log(error);
+    });
+}
+
+updateVisualization(5000, 2.5);
+
+d3.select("#updateButton").on("click", function() {
+    const newVoteThreshold = +d3.select("#voteInput").property("value");
+    const newRatingThreshold = +d3.select("#ratingInput").property("value");
+
+    if (!isNaN(newVoteThreshold) && !isNaN(newRatingThreshold)) {
+        updateVisualization(newVoteThreshold, newRatingThreshold);
+    } else {
+        alert("Ingresa parámetros válidos. Rating decimal usa punto");
+    }
+});
---
+++ rating_trend.js
@@ -0,0 +1,218 @@
+
+// Initialize the dimensions of the visualization
+const totalWidth = 3000;
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
+    width = totalWidth - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+// Append the svg object to the body of the page
+const svg = d3.select("#vis-2")
+  .append("svg")
+    .attr("width", totalWidth)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+// Read the data and draw the visualization
+d3.csv("anime.csv").then(function(data) {
+
+    let filteredData = data.filter(d => {
+    d.StartYear = +d.StartYear;
+    d.EndYear = +d.EndYear;
+    d["Rating Score"] = +d["Rating Score"];
+    return d.StartYear >= 1900 && d.StartYear <= new Date().getFullYear()
+            && d.EndYear >= 1900 && d.EndYear <= new Date().getFullYear()
+            && !isNaN(d["Rating Score"]);
+    });
+
+    const xDomain = d3.extent(filteredData, d => Math.min(d.StartYear, d.EndYear));
+    xDomain[0] -= 1;
+    const x = d3.scaleLinear()
+    .domain(xDomain)
+    .range([0, width]);
+
+    // Define Y axis scale
+    const y = d3.scaleLinear()
+    .domain([0, d3.max(filteredData, d => d["Rating Score"])])
+    .range([height, 0]);
+
+    // Add X axis
+    svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+
+    // Add Y axis
+    svg.append("g")
+    .call(d3.axisLeft(y));
+
+    // Create a tooltip
+    const tooltip = d3.select("#vis-2")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+        .style("position", "absolute");
+
+    function drawVisualization(numPoints) {
+        svg.selectAll("circle").remove();
+        svg.selectAll("line").remove();
+
+        let displayData = filteredData;
+        if (numPoints !== "all") {
+            displayData = filteredData.sort(() => 0.5 - Math.random()).slice(0, numPoints);
+        }
+
+        displayData.forEach(d => {
+            svg.append("line")
+            .attr("x1", x(d.StartYear))
+            .attr("y1", y(d["Rating Score"]))
+            .attr("x2", x(d.EndYear))
+            .attr("y2", y(d["Rating Score"]))
+            .attr("stroke", "red")
+            .attr("stroke-width", 1);
+        });
+
+        svg.selectAll("dot")
+            .data(displayData)
+            .enter()
+            .append("circle")
+            .attr("cx", d => x(d.StartYear))
+            .attr("cy", d => y(d["Rating Score"]))
+            .attr("r", 3)
+            .style("fill", "#2727bf")
+            .on("mouseover", function(event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html("Anime: " + d.Name + "<br/>Year: " + d.StartYear + "<br/>End Year: " + d.EndYear + "<br/>Rating: " + d["Rating Score"])
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 10) + "px");
+            })
+            .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+            });
+    }
+
+
+    drawVisualization("all");
+
+
+    d3.select("#dataCount").on("change", function() {
+        console.log("Dropdown changed");
+        const selectedValue = d3.select(this).property("value");
+        const numPoints = selectedValue === "all" ? "all" : parseInt(selectedValue, 10);
+        drawVisualization(numPoints);
+    });
+
+    d3.select("#filterButton").on("click", function() {
+        const yearInputString = d3.select("#yearInput").property("value");
+        let yearRange;
+    
+        if (yearInputString.includes("-")) {
+            yearRange = yearInputString.split("-").map(d => +d.trim());
+        } else {
+            const year = parseInt(yearInputString.trim(), 10);
+            if (!isNaN(year)) {
+                yearRange = [year, year];
+            }
+        }
+    
+
+        if (yearRange && yearRange.length === 2 && !isNaN(yearRange[0]) && !isNaN(yearRange[1])) {
+            const filteredYearsData = filteredData.filter(d => 
+                (d.StartYear >= yearRange[0] && d.StartYear <= yearRange[1]) ||
+                (d.EndYear >= yearRange[0] && d.EndYear <= yearRange[1]));
+    
+            updateVisualization(filteredYearsData);
+        } else {
+            alert("Porfavor ingresa en alguno de estos formatos: 2020 o 2013-2021).");
+        }
+    });
+
+    function updateVisualization(data) {
+        svg.selectAll("circle").remove();
+        svg.selectAll("line").remove();
+
+        data.forEach(d => {
+            svg.append("line")
+            .attr("x1", x(d.StartYear))
+            .attr("y1", y(d["Rating Score"]))
+            .attr("x2", x(d.EndYear))
+            .attr("y2", y(d["Rating Score"]))
+            .attr("stroke", "red")
+            .attr("stroke-width", 1);
+        });
+
+        svg.selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", d => x(d.StartYear))
+            .attr("cy", d => y(d["Rating Score"]))
+            .attr("r", 3)
+            .style("fill", "#2727bf")
+            .on("mouseover", function(event, d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html("Anime: " + d.Name + "<br/>Year: " + d.StartYear + "<br/>End Year: " + d.EndYear + "<br/>Rating: " + d["Rating Score"])
+                .style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 10) + "px");
+            })
+            .on("mouseout", function(d) {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+            });
+    }
+
+    // BRUSH
+    const brush = d3.brushX()
+        .extent([[0, 0], [width, height]])
+        .on("end", brushed);
+
+    svg.append("g")
+        .attr("class", "brush")
+        .call(brush);
+
+    function brushed(event) {
+        const selection = event.selection;
+        if (selection) {
+            const [x0, x1] = selection.map(x.invert);
+    
+            svg.selectAll("circle")
+                .style("opacity", 0.1)
+                .filter(d => d && x0 <= d.StartYear && d.StartYear <= x1)
+                .style("opacity", 1);
+    
+            svg.selectAll("line")
+                .style("opacity", 0.1)
+                .filter(d => d && x0 <= d.StartYear && d.StartYear <= x1)
+                .style("opacity", 1);
+        } else {
+            svg.selectAll("circle").style("opacity", 1);
+            svg.selectAll("line").style("opacity", 1);
+        }
+    }
+
+    // Right-click event listener to clear brush
+    svg.on("contextmenu", function(event) {
+    event.preventDefault();
+    d3.select(".brush").call(brush.move, null);
+    svg.selectAll("circle").style("opacity", 1);
+    svg.selectAll("line").style("opacity", 1);
+    });
+
+  
+}).catch(function(error){
+   console.log(error);
+});
+
+
